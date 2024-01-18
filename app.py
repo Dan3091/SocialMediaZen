9c27814dd9ea9c56a8ca910beb022db6963a0328
Dan <clarc.2007@gmail.com>
2024-01-18 17:45:08 +0200

    Created redirect_to_homepage to redirect your chrome browser to homepage

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import time
 import datetime
 from tkinter.messagebox import showinfo
 from tkinter import Tk
+import webbrowser
 
+
+message = "You've been on Facebook for more than 20 minutes, I'm sorry but that's enough, take a break!"
 # Create a function that acts as a countdown:
 def timer(h, m, s):
     total_sec = h * 3600 + m * 60 + s
@@ -16,18 +19,24 @@ def timer(h, m, s):
                 time_left_countdown = datetime.timedelta(seconds=total_sec)
                 time.sleep(1)
                 total_sec -= 1
+    return popup_message(message), redirect_to_homepage()
 
-    return popup_message("You've been on Facebook for more than 20 minutes, I'm sorry but that's enough, take a break!")
 #Create function that return the current app on focus:
 def current_app_on_focus():
     window_name = win32gui.GetWindowText(win32gui.GetForegroundWindow())
     return window_name
 
-#Create a function that call a popup window message
+#Create a function that call a popup window message:
 def popup_message(message):
     window = Tk()
     window.attributes('-topmost', 1)
     window.withdraw()
     return showinfo(title="Warning", message=message)
 
-timer(00, 00, 10)
+#Create a function to redirect to homepage:
+def redirect_to_homepage():
+    url = 'https://google.com'
+    webbrowser.register('chrome',
+                    None,
+                    webbrowser.BackgroundBrowser("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"))
+    webbrowser.get('chrome').open(url, new=0)
